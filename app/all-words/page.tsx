4fddbf7b4e96d9922662f7575b7d49a3b0d1e5e8
Roman2508 <2508romaa@gmail.com>
2024-07-29 11:16:50 +0300

    fix

--- app/all-words/page.tsx
+++ app/all-words/page.tsx
@@ -1,31 +1,68 @@
-import React from "react"
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+'use client'
+import React from 'react'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Pencil as EditIcon, Trash2 as DeleteIcon } from 'lucide-react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 const AllWordsPage = () => {
+  const [newWord, setNewWord] = React.useState('')
+  const [search, setSearch] = React.useState('')
+
   return (
-    <Table>
-      <TableCaption>A list of your recent not correct answers.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Array(7)
-          .fill(null)
-          .map((_, i) => (
-            <TableRow key={i}>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
-            </TableRow>
-          ))}
-      </TableBody>
-    </Table>
+    <>
+      <div className="flex items-center gap-3">
+        <div className="grid w-full items-center gap-1.5 mb-6">
+          <Label htmlFor="eng" className="mb-1">
+            Нове слово
+          </Label>
+          <Input id="eng" type="text" className="w-full" value={newWord} onChange={(e) => setNewWord(e.target.value)} />
+        </div>
+
+        <Button disabled={!newWord}>Додати</Button>
+      </div>
+
+      <div className="grid w-full items-center gap-1.5 mb-6">
+        <Label htmlFor="eng" className="mb-1">
+          Пошук
+        </Label>
+
+        <Input id="eng" type="text" className="w-full" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </div>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[60px] text-center p-2">№</TableHead>
+            <TableHead className="text-left p-2">Англійська</TableHead>
+            <TableHead className="text-left p-2">Українська</TableHead>
+            <TableHead className="text-center p-2">Дії</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {Array(7)
+            .fill(null)
+            .map((_, i) => (
+              <TableRow key={i}>
+                <TableCell className="font-medium p-2 text-center">{i + 1}</TableCell>
+                <TableCell className="p-2 text-left">Paid</TableCell>
+                <TableCell className="p-2 text-left">Credit Card</TableCell>
+                <TableCell className="flex gap-2 justify-center p-2">
+                  <Button variant="outline" size="icon">
+                    <EditIcon size={20} style={{ opacity: '.8' }} />
+                  </Button>
+
+                  <Button variant="outline" size="icon">
+                    <DeleteIcon size={20} style={{ opacity: '.8' }} />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </>
   )
 }
 
--- app/api/correct/route.ts
+++ app/api/correct/route.ts
@@ -2,13 +2,21 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/prisma/prisma-client'
 
 export async function GET() {
-  const words = await prisma.word.findMany()
+  const words = await prisma.correct.findMany()
   return NextResponse.json(words)
 }
 
 export async function POST(req: NextRequest) {
   const data = await req.json()
+
+  const existedWord = await prisma.correct.findFirst({ where: { eng: data.eng } })
+
+  console.log(existedWord)
+
+  if (existedWord) {
+    return new Response(`Таке слово вже існує в словнику`, { status: 400 })
+  }
+
   const word = await prisma.word.create({ data })
   return NextResponse.json(word)
 }
-
